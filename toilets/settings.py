890018fbce9e0fd51178a2da5e6abd870790f18e
Aleksey170999 <alexbooster3@yandex.ru>
2022-05-11 13:54:38 +0300

    settings for heroku

--- toilets/settings.py
+++ toilets/settings.py
@@ -1,25 +1,17 @@
 import os
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-75t51%9@)%$78=93$o&iu2qae1g4h2!=4c5_-g7yb4n4td^pc^'
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-DEBUG_PROPAGATE_EXCEPTIONS = True
 
 ALLOWED_HOSTS = [
-    'toilet-helper.herokuapp.com',
     '127.0.0.1',
+    'toilet-helper.herokuapp.com',
 ]
 
-# Application definition
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -70,35 +62,28 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'toilets.wsgi.application'
 
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-
-if not DEBUG:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': 'd750v0r1fjfo4e',
-            'USER': 'tjgffaxjgihheg',
-            'PASSWORD': 'd2d14606c062ec6fd6405f5133523dcb5f9ff70b87d11ee5cc8dc76c60d4d565',
-            'HOST': 'ec2-52-18-116-67.eu-west-1.compute.amazonaws.com',
-            'PORT': '5432',
-        }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'd750v0r1fjfo4e',
+        'USER': 'tjgffaxjgihheg',
+        'PASSWORD': 'd2d14606c062ec6fd6405f5133523dcb5f9ff70b87d11ee5cc8dc76c60d4d565',
+        'HOST': 'ec2-52-18-116-67.eu-west-1.compute.amazonaws.com',
+        'PORT': '5432',
     }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': 'toilets',
-            'USER': 'aleksejtihonov',
-            'PASSWORD': 'Dmesggrepeth1',
-            'HOST': '127.0.0.1',
-            'PORT': '5432',
-        }
-    }
+}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'toilets',
+#         'USER': 'aleksejtihonov',
+#         'PASSWORD': 'Dmesggrepeth1',
+#         'HOST': '127.0.0.1',
+#         'PORT': '5432',
+#     }
+# }
 
-# Password validation
-# https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -115,8 +100,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-# Internationalization
-# https://docs.djangoproject.com/en/3.2/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
 
@@ -141,8 +124,3 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
-try:
-    from local_settings import *
-except ImportError:
-    pass
