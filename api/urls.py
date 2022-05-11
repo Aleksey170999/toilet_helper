46f874e3a5d880d957c89c43ac7ee823fe868604
Aleksey170999 <alexbooster3@yandex.ru>
2022-05-11 17:33:52 +0300

    try to add media

--- api/urls.py
+++ api/urls.py
@@ -1,13 +1,19 @@
-from django.urls import path
+from django.conf.urls.static import static
+from django.urls import path, re_path
 from rest_framework.urlpatterns import format_suffix_patterns
-from . import views
 
+from django.conf import settings
+from . import views
+from django.views.static import serve
 
 urlpatterns = [
     path('toilets/list/', views.ToiletListView.as_view()),
     path('toilets/create/', views.ToiletCreateView.as_view()),
     path('toilets/list/<int:tg_id>/', views.ToiletListViewPersonolized.as_view()),
-
+    re_path(r'^media/(?P<path>.*)$', serve, {'deocument_root': settings.MEDIA_ROOT})
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
