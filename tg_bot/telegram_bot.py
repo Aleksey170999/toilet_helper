6b74a7c82b20c5ad89dd90f7e3ed36d19bc229d5
Aleksey170999 <alexbooster3@yandex.ru>
2022-05-11 12:16:42 +0300

    aaa

--- tg_bot/telegram_bot.py
+++ tg_bot/telegram_bot.py
@@ -1,4 +1,4 @@
-from aiogram import executor, types
+from aiogram import executor
 from bot_handlers import register_handlers_common
 from bot_config import dp
 
@@ -6,3 +6,16 @@ from bot_config import dp
 if __name__ == "__main__":
     register_handlers_common(dp)
     executor.start_polling(dp, skip_updates=True)
+
+
+# import time
+# import pyautogui
+#
+# time.sleep(3)
+#
+# x = 0
+# while x < 1000:
+#     pyautogui.press('1')
+#     pyautogui.press('esc')
+#     x += 1
+#     time.sleep(0.5)
--- toilets/settings.py
+++ toilets/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-75t51%9@)%$78=93$o&iu2qae1g4h2!=4c5_-g7yb4n4td^pc^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     'toilet-helper.herokuapp.com',
